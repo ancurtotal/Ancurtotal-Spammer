ff2d7cc2708be3266133202af77e6a4b335bae2e
Ekay Ancurtotal <41782306+ancurtotal@users.noreply.github.com>
2019-02-03 01:48:16 +0700

    Add files via upload

---
+++ Ancurtotal-Spammer.sh
@@ -0,0 +1,622 @@
+#!/bin/bash
+#version 1.0
+
+# MAU NGAPAIN JINK??
+# DI RECODE GAK BAKAL JADIIN GUE NEWBIE DAN LO MASTAH!!!
+# DI GANTI AUTHOR GAK BAKAL JADIIN GUE NOOB DAN LO PRO!!!
+# MIKIR PAKE DENGKUL BRO JANGAN PAKE OTAK!!!
+# JANGAN REEDIT ATAU RECODE KECUALI KALO LO GAK ADA OTAKNYA!!!
+# KALO LOE GAK PAHAM SILAHKAN BELAJAR BARENG SAMA GUE DI ANCURTOTAL CYBER TEAM!!!
+
+#02/02/19
+
+clear
+# Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+
+figlet TUAN B4DUT  | lolcat
+
+echo  "_____________________________________________________________" | lolcat
+echo  "[TYPE      : TOOLS INSTALLER] " |lolcat
+echo  "[VERSION   : V.1] " | lolcat
+echo  "[TOTALS    : 21 TOOLS] " | lolcat
+echo  "[AUTHOR    : EKAY ANCURTOTAL] " |lolcat
+echo  "[ASSOCIATE : ANCURTOTAL CYBER TEAM] " |lolcat
+echo  "[ASSOCIATE : SCARY OF CYBER DEMON] " |lolcat
+echo  "[ASSOCIATE : BUFT TEAM] " |lolcat
+echo  "[ASSOCIATE : MUSLIM CYBER ARMY] " |lolcat
+echo  "[NOTES     : JELAJAHI SETIAP TOOLS DENGAN BIJAK] " | lolcat
+echo  "[THANKS TO : ALLAH SWT AND RASULULLAH SAW]" | lolcat
+echo  "[THANKS TO : MY WIFE AND TWO DAUGHTERS]" | lolcat
+echo  "_____________________________________________________________" | lolcat
+
+sleep 1
+
+###################################################
+# CTRL + C
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo  $green"TOOLS YANG KALIAN INSTALL ADA DI DIRECTORY TOOLS GUE " |lolcat
+echo  $green"Thanks For Using My Tools" |lolcat
+sleep 1
+echo  $green"EKAY ANCURTOTAL On Your System" |lolcat
+
+echo  $green"See you Again SCRIPTKIDDIES😎 :)..." |lolcat
+echo  $green"AIM FOR SOMETHING THAT IS IMPOSSIBLE"
+echo  $green"THERE IS NO SAFE SYSTEM"
+echo  $green"HAVE FUN IN CYBERSPACE AND THE REAL WORLD"
+echo  $green"HACKING IS NOT ALWAYS A CRIME :v"
+figlet ANCURTOTAL | lolcat
+echo "                 #  ANCURTOTAL CYBER TEAM # " | lolcat
+sleep 1
+exit
+}
+
+
+lagi=1
+while [ $lagi -lt 6 ];
+do
+
+echo ""                
+echo ""                
+
+echo  "######################################" | lolcat
+echo  "#SILAHKAN TUNGGU DAN LIHAT LIST TOOLS#" | lolcat
+echo  "######################################" | 
+lolcat
+
+echo ""                
+echo  "TOOLS INI CUMAN UNTUK MENGINSTALLNYA SAJA" |lolcat
+echo  "JIKA MAU MENJALANKAN TOOLS YANG KALIAN INSTALL" | lolcat
+echo  "SILAHKAN KETIK 00 LALU KETIK ls" | lolcat
+echo  "DAN MASUK KE TOOLS YANG KALIAN INSTALL" | lolcat
+echo  "JANGAN NANYA LAGI JINK " | lolcat
+echo  "SABAR YA NUNGGU NYA :v " | lolcat
+echo  "NGOPI!!! " | lolcat
+echo  "#TTD EKAY ANCURTOTAL 😎" | lolcat
+echo  "#ORANG GILA MAH BEBAS😎" | lolcat
+echo ""  
+echo ""  
+
+
+echo "============================" | lolcat
+echo "1. Install LITESPAM${enda}";
+echo "============================" | lolcat
+echo "2. Install Bom SMS All Operator${enda}";
+echo "============================" | lolcat
+echo "3. Install Bom SMS Tri${enda}";
+echo "============================" | lolcat
+echo "4. Install Santet-Online${enda}";
+echo "============================" | lolcat
+echo "5. Install CnkSpam${enda}";
+echo "============================" | lolcat
+echo "6. Install RusSpam${enda}";
+echo "============================" | lolcat
+echo "7. Install SpazSMS${enda}";
+echo "============================" | lolcat
+echo "8. Install New-Spammer${enda}";
+echo "============================" | lolcat
+echo "9. Install Spam${enda}";
+echo "============================" | lolcat
+echo "10. Install QJDID${enda}";
+echo "============================" | lolcat
+echo "11. Install QFloodSms${enda}";
+echo "============================" | lolcat
+echo "12. Install Spam-Mantan${enda}";
+echo "============================" | lolcat
+echo "13. Install AVARSpam${enda}";
+echo "============================" | lolcat
+echo "14. Install ANRSpam${enda}";
+echo "============================" | lolcat
+echo "15. Install MultiSpam${enda}";
+echo "============================" | lolcat
+echo "16. Install SpamChat${enda}";
+echo "============================" | lolcat
+echo "17. Install GCOspam${enda}";
+echo "============================" | lolcat
+echo "18. Install Spammer-Grab${enda}";
+echo "============================" | lolcat
+echo "19. Install Spam Boms Tsel${enda}";
+echo "============================" | lolcat
+echo "20. Install Spam Boms Mall${enda}";
+echo "============================" | lolcat
+echo "21. Install Egyptian-SMS-Spammer${enda}";
+echo "============================" | lolcat
+echo "00. Exit${enda}";
+echo "============================" | lolcat
+echo "     [PILIH NOMORNYA BRO]"
+echo
+read -p "Ancurtotal@Spammer ~>>" pil;
+
+#Install LITESPAM
+
+case $pil in
+1) clear
+echo "Install LITESPAM...Installing, Please Wait !!" | lolcat
+sleep 1
+pkg update
+pkg upgrade
+pkg install python2
+pkg install php
+pkg install toilet
+pkg install sh
+pkg install git
+git clone https://github.com/4L13199/LITESPAM
+echo
+echo "SUDAH TERINSTALL BRO!!" | lolcat
+echo
+echo "Menu LITESPAM :" | lolcat
+echo " 1. Tokopedia" | lolcat
+echo " 2. Telkomsel" | lolcat
+echo " 3. Matahari Mall" | lolcat
+echo " 4. PHD" | lolcat
+echo " 5. Jd.Id" | lolcat
+echo " 6. Email Bomber" | lolcat
+echo " 0. Keluar" | lolcat
+echo
+echo "Cara Menggunakan LITESPAM :"
+echo " $ cd LITESPAM"
+echo " $ chmod +x LITESPAM.sh"
+echo " $ sh LITESPAM.sh"
+
+
+;;
+
+#Install Bom SMS All Operator
+
+2) clear
+echo "Install Bom SMS All Operator...Installing, Please Wait !!" | lolcat
+sleep 1 
+pkg update
+pkg upgrade
+pkg install python2
+pkg install php
+pkg install git
+git clone https://github.com/rootnet007/bom.git
+echo
+echo "SUDAH TERINSTALL BRO!!" | lolcat
+echo
+echo "Cara Menggunakan Bom SMS All Operator:"
+echo " $ cd bom"
+echo " $ chmod +x bom.php"
+echo " $ php bom.php"
+echo
+echo "- Masukkan Nomor Target..."
+echo "- Masukan Jumlah Input..."
+echo "- Masukan Jumlah Kecepatan..."
+echo "- Lalu Tunggu Hingga Proses Selesai..."
+
+
+;;
+
+#Install Bom SMS Tri
+
+3) clear
+echo "Install Bom SMS Tri...Installing, Please Wait !!" | lolcat
+sleep 1
+pkg update
+pkg upgrade
+pkg install python2
+pkg install php
+pkg install git
+git clone https://github.com/bayufedra/bom-sms-otp-tri/
+echo
+echo "SUDAH TERINSTALL BRO!!" | lolcat
+echo
+echo "Cara Menggunakan Bom SMS Tri :"
+echo " $ cd bom-sms-otp-tri"
+echo " $ chmod +x bom-tri.php"
+echo " $ php bom-tri.php"
+echo
+echo "- Masukkan Nomor Target..."
+echo "- Masukan Limit..."
+echo "- Masukan Jumlah Kecepatan..."
+echo "- Lalu Tunggu Hingga Proses Selesai..."
+
+
+;;
+
+#Install Santet-Online
+
+4) clear
+echo "Install Santet-Online...Installing, Please Wait !!" | lolcat
+sleep 1
+apt update
+apt upgrade
+apt install python2
+pip2 install requests
+apt install git
+git clone https://github.com/Gameye98/santet-online
+echo
+echo "SUDAH TERINSTALL BRO!!" | lolcat
+echo
+echo "Menu LITESPAM :" | lolcat
+echo " 1. Create a Netcat Payload and Listener" | lolcat
+echo " 2. Facebook Group Hijack Attack" | lolcat
+echo " 3. SMS Bomber Attack Vecktor" | lolcat
+echo " 4. SMS Proof Attack Vecktor" | lolcat
+echo " 5. Denial-of-Service Attack" | lolcat
+echo " 00. Exit The Santet" | lolcat
+echo
+echo "Cara Menggunakan Santet-Online :"
+echo " $ cd santet-online"
+echo " $ chmod +x santet.py"
+echo " $ python2 santet.py"
+
+
+;;
+
+#install CnkSpam
+
+5) clear
+echo "Install CnkSpam...Installing, Please Wait !!" | lolcat
+sleep 1
+pkg update
+pkg upgrade
+pkg install python2
+pkg install php
+pkg install sh
+pkg install git
+git clone https://github.com/hatakecnk/cnkspam
+echo
+echo "SUDAH TERINSTALL BRO!!" | lolcat
+
+
+;;
+
+#Install RusSpam
+
+6) clear
+echo "Install RusSpam...Installing, Please Wait !!" | lolcat
+sleep 1
+pkg update
+pkg upgrade
+pkg install python2
+pkg install php
+pkg install sh
+pkg install git
+git clone https://github.com/Rusmana-ID/rus
+echo
+echo "SUDAH TERINSTALL BRO!!" | lolcat
+
+
+;;
+
+#Install SpazSMS
+
+7) clear
+echo "Install SpazSMS...Installing, Please Wait !!" | lolcat
+sleep 1
+pkg update
+pkg upgrade
+pkg install python2
+pkg install php
+pkg install sh
+pkg install git
+git clone https://github.com/Gameye98/SpazSMS
+echo
+echo "SUDAH TERINSTALL BRO!!" | lolcat
+
+
+;;
+
+# Install New-Spammer
+
+8) clear
+echo "Install New-Spammer...Installing, Please Wait !!" | lolcat
+sleep 1
+pkg update
+pkg upgrade
+pkg install python2
+pkg install php
+pkg install sh
+pkg install git
+git clone https://github.com/haijuga7/New-Spammer
+echo
+echo "SUDAH TERINSTALL BRO!!" | lolcat
+
+
+;;
+
+#Install Spam
+
+9) clear
+echo "Install Spam...Installing, Please Wait !!" | lolcat
+sleep 1
+pkg update
+pkg upgrade
+pkg install python2
+pkg install php
+pkg install sh
+pkg install git
+git clone https://github.com/SIIL3NT/spam
+echo
+echo "SUDAH TERINSTALL BRO!!" | lolcat
+
+
+;;
+
+#Install QJDID
+
+10) clear
+echo "Install QJDID...Installing, Please Wait !!" | lolcat
+sleep 1
+pkg update
+pkg upgrade
+pkg install python2
+pkg install php
+pkg install sh
+pkg install git
+git clone https://github.com/QiubyZ/QJDID
+echo
+echo "SUDAH TERINSTALL BRO!!" | lolcat
+
+
+;;
+
+#Install QFloodSms
+
+11) clear
+echo "Install QFloodSms...Installing, Please Wait !!" | lolcat
+sleep 1
+pkg update
+pkg upgrade
+pkg install python2
+pkg install php
+pkg install sh
+pkg install git
+git clone https://github.com/QiubyZ/QFloodSms
+echo
+echo "SUDAH TERINSTALL BRO!!" | lolcat
+
+
+;;
+
+#Install Spam-Mantan
+
+12) clear
+echo "Install Spam-Mantan...Installing, Please Wait !!" | lolcat
+sleep 1
+pkg update
+pkg upgrade
+pkg install python2
+pkg install php
+pkg install sh
+pkg install git
+git clone https://github.com/not404foundcyberteam/spam-mantan
+echo
+echo "SUDAH TERINSTALL BRO!!" | lolcat
+
+
+;;
+
+#Install AVARspam
+
+13) clear
+echo "Install AVARspam...Installing, Please Wait !!" | lolcat
+sleep 1
+pkg update
+pkg upgrade
+pkg install python2
+pkg install php
+pkg install sh
+pkg install git
+git clone https://github.com/ALX-04/AVARspam
+echo
+echo "SUDAH TERINSTALL BRO!!" | lolcat
+
+
+;;
+
+#Install ANRspamm
+
+14) clear
+echo "Install ANRspam...Installing, Please Wait !!" | lolcat
+sleep 1
+pkg update
+pkg upgrade
+pkg install python2
+pkg install php
+pkg install sh
+pkg install git
+git clone https://github.com/Amriez/ANRspam
+echo
+echo "SUDAH TERINSTALL BRO!!" | lolcat
+
+
+;;
+
+#Install MultiSpam
+
+15) clear
+echo "Install MultiSpam...Installing, Please Wait !!" | lolcat
+sleep 1
+pkg update
+pkg upgrade
+pkg install python2
+pkg install php
+pkg install sh
+pkg install git
+git clone https://github.com/kereh/MultiSpam
+echo
+echo "SUDAH TERINSTALL BRO!!" | lolcat
+
+
+;;
+
+#Install SpamChat
+
+16) clear
+echo "Install SpamChat...Installing, Please Wait !!" | lolcat
+sleep 1
+pkg update
+pkg upgrade
+pkg install python2
+pkg install php
+pkg install sh
+pkg install git
+git clone https://github.com/Senitopeng/Spamchat
+echo
+echo "SUDAH TERINSTALL BRO!!" | lolcat
+
+
+;;
+
+#Install Spam GCOspam
+
+17) clear
+echo "Install Spam GCOspam...Installing, Please Wait !!" | lolcat
+sleep 1
+apt update
+apt upgrade
+apt install python2
+pkg install php
+pkg install sh
+apt install git
+git clone https://github.com/Amriez/gcospam
+echo
+echo "SUDAH TERINSTALL BRO!!" | lolcat
+echo
+echo "Cara Menggunakan LITESPAM :"
+echo " $ cd gcospam"
+echo " $ sh install.sh"
+echo " $ sh gco.sh"
+echo
+echo "- Pilih Nomor Yang Mana Aja...Bebas !!!"
+echo "- Masukan Nomor Target Tanpa 0/62...."
+echo "- Input Bebas"
+echo "- Jeda Default Aja"
+echo "- Lalu Tunggu Hingga Proses Selesai..."
+
+
+;;
+
+#Install Spammer-Grab
+
+18) clear
+echo "Install Spammer-Grab...Installing, Please Wait !!" | lolcat
+sleep 1
+pkg update
+pkg upgrade
+pkg install python2
+pip2 install requests
+pkg install git
+git clone https://github.com/p4kl0nc4t/Spammer-Grab
+echo
+echo "SUDAH TERINSTALL BRO!!" | lolcat
+echo
+echo "Cara Menggunakan Spammer-Grab :"
+echo " $ cd Spammer-Grab"
+echo " $ chmod +x spammerv1.py"
+echo " $ python2 spammerv1.py"
+echo " $ python2 spammerv1.py phone" 
+echo
+echo "Contoh :"
+echo " $ python2 spammerv1.py 6283831919278 "
+echo "- Lalu Tunggu Hingga Proses Selesai..."
+
+
+;;
+
+#Install Spam Boms Tsel
+
+19) clear
+echo "Install Spam Boms Tsel...Installing, Please Wait !!" | lolcat
+sleep 1
+pkg update
+pkg upgrade
+pkg install curl
+pkg install php
+curl -s http://files-store.theprivat.ml/uploads/bombtsel.txt > bombtsel.php 
+echo
+echo "SUDAH TERINSTALL BRO!!" | lolcat
+echo
+echo "Cara Menggunakan Spam Boms Tsel :"
+echo " $ chmod 777 bombtsel.php"
+echo " $ php bombtsel.php"
+
+
+;;
+
+#Install Spam Bom Mall
+
+20) clear
+echo "Install Spam Bom Mall...Installing, Please Wait !!" | lolcat
+pkg install update
+pkg install upgrade
+pkg install wget
+pkg install php
+wget http://files-store.theprivat.ml/uploads/bom-mall.zip
+unzip bom-mall.zip
+echo
+echo "SUDAH TERINSTALL BRO!!" | lolcat
+echo "Cara Menggunakan Spam Bom Mall :"
+echo " $ cd bom-mall"
+echo " $ php run.php"
+
+
+;;
+
+#Install Egyptian-SMS-Spammer
+
+21) clear
+echo "Install Egyptian-SMS-Spammer...Installing, Please Wait !!" | lolcat
+pkg install update
+pkg install upgrade
+pkg install wget
+pkg install php
+https://github.com/WazeHell/Egyptian-SMS-Spammer
+echo
+echo "SUDAH TERINSTALL BRO!!" | lolcat
+echo
+echo "Cara Menggunakan Egyptian-SMS-Spammer :"
+echo " $ cd Egyptian-SMS-Spammer"
+echo " $ chmod 777 run.py"
+echo " $ python2 run.py"
+echo
+echo "- Masukkan Nomor Target..."
+echo "- Lalu Tunggu Hingga Proses Selesai..."
+
+
+;;
+
+00) lolcat ThanksYou.txt
+echo
+
+echo  $green"TOOLS YANG KALIAN INSTALL ADA DI DIRECTORY TOOLS GUE " |lolcat
+echo  $green"Thanks For Using My Tools" |lolcat
+sleep 1
+echo  $green"EKAY ANCURTOTAL On Your System" |lolcat
+
+echo  $green"See you Again SCRIPTKIDDIES😎 :)..." |lolcat
+echo  $green"AIM FOR SOMETHING THAT IS IMPOSSIBLE"
+echo  $green"THERE IS NO SAFE SYSTEM"
+echo  $green"HAVE FUN IN CYBERSPACE AND THE REAL WORLD"
+echo  $green"HACKING IS NOT ALWAYS A CRIME :v"
+figlet ANCURTOTAL | lolcat
+echo "                 #  ANCURTOTAL CYBER TEAM # " | lolcat
+exit
+
+
+;;
+
+*) echo "Sorry, your choice isn't right ... drink coffee first bro :) "
+esac
+done
+done
+
